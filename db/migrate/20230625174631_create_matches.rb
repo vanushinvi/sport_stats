36b149b8b3a0a714063e0e38d7a5f430b61c0840
Vladimir Vanyushin <vladimir.vanyushin@dualbootpartners.com>
2023-06-25 22:18:07 +0400

    add migrations

---
+++ db/migrate/20230625174631_create_matches.rb
@@ -0,0 +1,10 @@
+class CreateMatches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :matches do |t|
+      t.references :home_team, null: false, foreign_key: { to_table: :teams }
+      t.references :away_team, null: false, foreign_key: { to_table: :teams }
+
+      t.timestamps
+    end
+  end
+end
